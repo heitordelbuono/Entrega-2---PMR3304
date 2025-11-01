74cea92b739b5ac82de78a1e8f5c06bc7d5f344d
Heitor Del Buono <heitordelbuono@usp.br>
2025-11-01 18:45:13 -0300

    0.0

---
+++ staticpages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'staticpages/index.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'staticpages/about.html', context)
