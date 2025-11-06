8f2ea1adf856267588e765cce1418f920f46c63a
Heitor Del Buono <heitordelbuono@usp.br>
2025-11-06 10:38:35 -0300

    Ajuste para rodar no Render

--- E2Sisinfo/settings.py
+++ E2Sisinfo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os 
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +76,29 @@ WSGI_APPLICATION = 'E2Sisinfo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+IS_RENDER = os.environ.get('RENDER', False)
+
+if IS_RENDER:
+    # Se estiver no Render:
+    # 1. Leia a URL do banco de dados da variável de ambiente DATABASE_URL
+    # 2. Configure o SSL (necessário para o Render)
+    print("Estamos no Render, usando PostgreSQL...")
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    # Se estiver Local (seu PC):
+    # 1. Use o SQLite de sempre
+    print("Estamos localmente, usando SQLite...")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
